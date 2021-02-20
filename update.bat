94a0d9ef366a3c1ecc8c346db116c8e2ab3bc48f
Maddonix <thomas.lux@empolis.com>
2021-02-20 14:39:01 +0100

    .

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 :: Setup batch file to install conda environment
 TITLE Setup Conda
-ECHO You are about to install the conda environment "mistos", please make sure no environment with the same name exists.
+ECHO You are about to update the conda environment "mistos", please make sure you already installed Mistos (setup.bat).
 PAUSE
 call conda activate mistos
 call conda env update --file environment.yml
