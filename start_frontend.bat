47cb126f096a97d0719add2d9ce47699f606162e
Thomas Lux <thomas.lux@empolis.com>
2021-02-20 12:18:56 +0100

    .

--- start_frontend.bat
+++ start_frontend.bat
@@ -2,5 +2,5 @@
 :: Simple python http server to serve frontend at port 4200
 TITLE Mistos Frontend
 ECHO Mistos Frontend serving at port 4200.
-conda activate mistos
+call conda activate mistos
 call python frontend_server.py
